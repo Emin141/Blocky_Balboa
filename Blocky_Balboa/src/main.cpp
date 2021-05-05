16a061514e83b0649dec4d7e811484dfb4710c58
Emin Turalic <emin141@outlook.com>
2021-05-05 22:25:26 +0200

    Changed wavefront object file extentions in the source code.

--- Blocky_Balboa/src/main.cpp
+++ Blocky_Balboa/src/main.cpp
@@ -14,14 +14,14 @@ int main() {
 	g_MVP = player.getMVP();
 
 	Entity donut1(
-		"./res/models/donut.vmf",
+		"./res/models/donut.wfo",
 		"./res/shaders/donut.vert",
 		"./res/shaders/donut.frag"
 	);
 	donut1.setWorldPosition({ 0.0f, 1.0f, -10.f });
 
 	Entity terrain(
-		"./res/models/flatland.vmf",
+		"./res/models/flatland.wfo",
 		"./res/shaders/flatland.vert",
 		"./res/shaders/flatland.frag"
 	);
